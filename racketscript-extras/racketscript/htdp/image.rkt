c9c7a4e4910e7d29fa845af89fbf9d436b22f23e
Ayden Diel <aydendiel@gmail.com>
2023-04-11 10:33:03 -0400

    Added overlay to (empty-scene) in image.rkt

--- racketscript-extras/racketscript/htdp/image.rkt
+++ racketscript-extras/racketscript/htdp/image.rkt
@@ -268,9 +268,10 @@
              (#js.ctx.lineTo (posn-x pt) (posn-y pt))
              (loop (cdr points)))))))])
 
-(define (empty-scene width height)
-  (rectangle width height "solid" "white")
-  (rectangle width height "outline" "black"))   
+(define (empty-scene width height [color "white"])
+  (overlay
+    (rectangle width height "solid" color)
+    (rectangle width height "outline" "black")))
 
 (define (text txt size color)
   (new (Text txt
